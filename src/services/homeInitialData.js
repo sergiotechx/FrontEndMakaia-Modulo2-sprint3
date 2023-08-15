96e370166535dbaa991ec45f86683f44b17755b8
sergiotechx <sergiotechx@yahoo.com>
2023-08-14 23:22:04 -0500

    cambios

--- src/services/homeInitialData.js
+++ src/services/homeInitialData.js
@@ -5,33 +5,37 @@ export async function getHomeInitialData(data) {
         let messages = []
         let followingUsers = []
 
-       
-        let tempMessages = await getPosts(data.id)
+        for (let index = 0; index < data.following.length; index++) {
+            let tempUser = await getUser(data.following[index].id)
 
+            if (Object.keys(tempUser).length > 0) {
+                followingUsers.push(tempUser)
+            }
+        }
+
+        let tempMessages = await getPosts(data.id)
+        console.log("la datisa", data)
         tempMessages.forEach(element => {
-            messages.push(element)
+            messages.push({ message: element, user: data })
         });
-        
+
+
+
         for (let index = 0; index < data.following.length; index++) {
             tempMessages = await getPosts(data.following[index].id)
-
+            let tempUser = followingUsers.find(user=>user.id==data.following[index].id)
             tempMessages.forEach(element => {
-                messages.push(element)
+                messages.push({ message: element, user: tempUser })
             });
         }
 
-        for(let index = 0; index < data.following.length; index++){
-           let  tempUser = await getUser(data.following[index].id)
-       
-            if(Object.keys(tempUser).length > 0){
-                followingUsers.push(tempUser) 
-            }
-        }
-        messages.sort((a,b)=>b.timestamp-a.timestamp)
 
-        
-      
-        return ({messages, followingUsers})
+        messages.sort((a, b) => b.message.timestamp - a.message.timestamp)
+       
+
+
+
+        return ({ messages, followingUsers })
 
     }
     catch (error) {
@@ -43,9 +47,9 @@ const getPosts = async (id) => {
     try {
         let complete_Url = Data_URL + `posts?userId=${id}`
         const tempPosts = await axios.get(complete_Url)
-        
+
         let messages = []
-                           
+
         if (tempPosts.data.length > 0) {
             messages = tempPosts.data
         }
@@ -57,14 +61,14 @@ const getPosts = async (id) => {
 }
 const getUser = async (id) => {
     try {
-     
+
         let complete_Url = Data_URL + `users/${id}`
         const tempUserData = await axios.get(complete_Url)
         let user = {}
-        
+
         if (Object.keys(tempUserData.data).length > 0 > 0) {
             user = tempUserData.data
-            
+
         }
         return user
     }
