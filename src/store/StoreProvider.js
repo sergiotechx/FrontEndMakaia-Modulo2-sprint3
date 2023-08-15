9818dbd66b22425fce71733008b2e597c16c74ce
Alexis Garcia <alexis25yo@gmail.com>
2023-08-15 15:29:14 -0500

    added perfil

--- src/store/StoreProvider.js
+++ src/store/StoreProvider.js
@@ -1,22 +1,27 @@
-'use client'
+"use client";
 import { createContext, useReducer } from "react";
 import authReducer, { initialAuthStore } from "./AuthReducer";
 import usersReducer, { initialUsersStore } from "./UsersReducer";
 import userReducer, { initialUserStore } from "./UserReducer";
-
+import userReducer, { initialUserStore } from "./PerfilReducer";
 const StoreContext = createContext();
 
 const StoreProvider = ({ children }) => {
-    const [authStore, authDispatch] = useReducer(authReducer, initialAuthStore)
-    const [usersStore, usersDispatch] = useReducer(usersReducer, initialUsersStore)
-    const [userStore, userDispatch] = useReducer(userReducer, initialUserStore)
-    return (
-        <StoreContext.Provider value={[authStore, authDispatch,usersStore,usersDispatch,userStore, userDispatch]}>
-            {children}
-        </StoreContext.Provider> 
-    )
-}
+  const [authStore, authDispatch] = useReducer(authReducer, initialAuthStore);
+  const [perfilStore, perfilDispatch] = useReducer(
+    userReducer,
+    initialUserStore
+  );
 
-export { StoreContext}
+  return (
+    <StoreContext.Provider
+      value={[authStore, authDispatch, perfilStore, perfilDispatch]}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
-export default StoreProvider 
+export { StoreContext };
+
+export default StoreProvider;
