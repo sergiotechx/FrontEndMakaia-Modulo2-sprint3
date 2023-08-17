d6715984a7736235d3ac01c7f8c5e1f820637ed4
sergiotechx <sergiotechx@yahoo.com>
2023-08-16 20:31:54 -0500

    .

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,16 +1,16 @@
-export const types = {
-    authLogin: 'auth - login',
-    authLogout: 'auth - logout',
-    perfilsetData: "set - data",
-    setComment : "set-comment",
-}
-
 const Data_URL = 'https://frontendmakaia-modulo2-sprint3-jsonserver.onrender.com/'
-const URL_API = 'http://localhost:3001'
 const  Session_Name ='Findy'
+const URL_API = 'http://localhost:3001'
 
 const endpoints = {
     coments : `${URL_API}/comments`
 }
 
 export {Data_URL,Session_Name, endpoints}  
+ 
+export const types = {
+    authLogin: 'auth - login',
+    authLogout: 'auth - logout',
+    perfilsetData: "set - data",
+    setComment : "set-comment",
+}
