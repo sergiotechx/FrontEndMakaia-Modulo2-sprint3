10f95cbc1a322880844cceaf6e0ad4b88a721b50
Alexis Garcia <alexis25yo@gmail.com>
2023-08-18 16:52:24 -0500

    added rutes

--- src/app/(pages)/perfil/page.jsx
+++ src/app/(pages)/perfil/page.jsx
@@ -1,16 +1,77 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./page.scss";
 import { StoreContext } from "@/store/StoreProvider";
+import { Button } from "@mantine/core";
+import Swal from "sweetalert2";
 
 const Page = () => {
   const { perfilStore } = useContext(StoreContext);
   const followersCount = perfilStore?.followers.length;
   const followingCount = perfilStore?.following.length;
   const fotos = perfilStore.posts[0].message.image;
+  const [menuOptions, setMenuOptions] = useState(false);
+  const [disabeled, setDisabeled] = useState(true);
+  const [inputValue, setInputValue] = useState(perfilStore.name);
+
+  const handleBack = () => {
+    console.log("Atras");
+  };
+
+  const handleOptions = () => {
+    setMenuOptions((prevMenuOptions) => !prevMenuOptions);
+  };
+
+  const handleEdit = () => {
+    setDisabeled((prevDisabeled) => !prevDisabeled);
+    setMenuOptions(false);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleClickEdit = () => {
+    console.log("icon edit");
+    Swal.fire({
+      title: "Espera un momento!",
+      text: "Estas seguro de modificar tu perfil?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, modificar perfil!",
+    }).then((result) => {
+      console.log(inputValue);
+      if (result.isConfirmed) {
+        setDisabeled((prevDisabeled) => !prevDisabeled);
+        Swal.fire("Excelente!", "Perfil modificado con exito.", "success");
+      }
+    });
+  };
 
   return (
     <div className="firstContainer">
+      <figure className="options">
+        <img
+          onClick={handleBack}
+          src="/images/chevron-back-outline.svg"
+          alt=""
+        />
+        <img
+          onClick={handleOptions}
+          src="/images/ellipsis-horizontal-outline.svg"
+          alt=""
+        />
+      </figure>
+      <Button
+        onClick={handleEdit}
+        className={`menuOpcions ${menuOptions ? "menuOpcionsActive" : ""}`}
+        variant="gradient"
+        gradient={{ from: "#ed6ea0", to: "#ec8c69", deg: 35 }}
+      >
+        Editar perfil
+      </Button>
       <figure className="firstContainer__imgPortada">
         <img src={perfilStore.avatar} alt="" />
       </figure>
@@ -27,7 +88,21 @@ const Page = () => {
           </span>
         </div>
         <div className="firstContainer__description">
-          <h2>{perfilStore.name}</h2>
+          <div className="container__input">
+            <input
+              onChange={handleInputChange}
+              disabled={disabeled}
+              className="span"
+              value={inputValue}
+            ></input>
+            <figure
+              onClick={handleClickEdit}
+              className={`icon-edit  ${!disabeled ? "icon-editActive" : ""}`}
+            >
+              <img src="/images/edit-3.svg" alt="" />
+            </figure>
+          </div>
+
           <span>J. Hello Guys</span>
           <span>Follow me and like my post</span>
         </div>
--- src/services/comments.js
+++ src/services/comments.js
@@ -1,31 +1,32 @@
 import axios from "axios";
-import { endpoints} from "../constants/Constants";
-const julian = require('julian');
+import { endpoints } from "../constants/Constants";
+const julian = require("julian");
 
 const sendComent = async (newComent) => {
-    const now = new Date();
-    const newComentObject = { 
-        postId: 6, 
-        userId: 2, 
-        text: newComent,
-        timestamp: Number(julian(now)),
-    };
+  const now = new Date();
+  const newComentObject = {
+    postId: 6,
+    userId: 2,
+    text: newComent,
+    timestamp: Number(julian(now)),
+  };
 
-
-    try {
-              
-       
-            let result = await axios.post(endpoints.coments, JSON.stringify(newComentObject), {
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json'
-                }
-            })
-            console.log(result);
-            console.log('revisar', endpoints.coments);
-    } catch (error) {
-        console.error("Error al enviar el comentario:", error);
-    }
+  try {
+    let result = await axios.post(
+      endpoints.coments,
+      JSON.stringify(newComentObject),
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(result);
+    console.log("revisar", endpoints.coments);
+  } catch (error) {
+    console.error("Error al enviar el comentario:", error);
+  }
 };
 
 export { sendComent };
